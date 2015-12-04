64e2a0eccd8c933b15041a61f78d602564063b37
Hondallica <hondallica@gmail.com>
2015-12-04 20:27:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::BABYMETAL - It's new $module
+
+# SYNOPSIS
+
+    use Acme::BABYMETAL;
+
+# DESCRIPTION
+
+Acme::BABYMETAL is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica &lt;hondallica@gmail.com>
---
+++ lib/Acme/BABYMETAL.pm
@@ -0,0 +1,39 @@
+package Acme::BABYMETAL;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::BABYMETAL - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::BABYMETAL;
+
+=head1 DESCRIPTION
+
+Acme::BABYMETAL is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
